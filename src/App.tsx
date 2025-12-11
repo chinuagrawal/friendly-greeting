4881b615f03a7dba9f37c0ec406a31788577070d
Chinmay Agrawal <thechinmayagrawal@gmail.com>
2025-12-12 03:47:30 +0530

    added github base

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+         <BrowserRouter basename="/friendly-greeting">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
